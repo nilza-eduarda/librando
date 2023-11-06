d3f262c6dc5d777517846755c6d5c8a4dbc3e315
Nilza Eduarda <nilza.eduarda.queiroz@gmail.com>
2023-11-06 01:13:17 -0300

    versão final

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -20,3 +20,22 @@ function toggleSubMenu() {
     isSubMenuOpen = false;
   }
 }
+
+const handleClickOutside = (event) => {
+  let overlay = document.getElementById("overlay");
+  let modal = document.getElementById("modal");
+
+  if (!modal.contains(event.target)) {
+      modal.style.display = 'none';
+      overlay.style.display = 'none';
+      document.removeEventListener('click', handleClickOutside, false);
+  }
+}
+
+const openModal = () => {
+  let overlay = document.getElementById("overlay");
+  let modal = document.getElementById("modal");
+  overlay.style.display = 'flex'
+  modal.style.display = 'flex'
+  setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
+}
